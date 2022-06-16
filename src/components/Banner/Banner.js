c3387127774751fdd549dee6ad9fe11a484a2177
Evan Anderson <evndanderson@gmail.com>
2022-06-16 12:16:46 -0500

    Simplified banner CSS selectors

--- src/components/Banner/Banner.js
+++ src/components/Banner/Banner.js
@@ -3,8 +3,8 @@ import './style.css'
 export default function Banner(props) {
     return (
         <div className={`banner ${props.type}`}>
-            <img className={`banner-image ${props.type}`} src={props.src} alt={props.alt} style={props.imageStyle}></img>
-            <div className={`banner-description ${props.type}`}>
+            <img className='image' src={props.src} alt={props.alt}></img>
+            <div className='description'>
                 <header><h2>{props.title}</h2></header>
                 <p>{props.description}</p>
                 <br/>
